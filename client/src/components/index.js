3c9190c36a1c52a2c8b1c7c0570a1944f1b1abb5
Saymoregeomancer <58919347+Saymoregeomancer@users.noreply.github.com>
2023-10-09 12:36:05 +0300

    Delete client/src/components directory

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import AddLink from "./AddLink/AddLink";
-import Authorization from "./Auth/Authorization";
-import ShopLIst from "./ShopLIst/ShopLIst";
-import ProductsList from "./ProductsList/ProductsList";
-import Filter from "./Filter/Filter";
-import Navigation from "./Navigation/Navigation";
-export { Authorization, AddLink, Navigation, Filter, ProductsList, ShopLIst };
